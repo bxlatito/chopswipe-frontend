956294ecea8966c629d9898fbf0e4556d885b3ea
Bolatito Olabode <bolaoluwatitoolabode@gmail.com>
2026-06-04 14:11:34 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import './App.css'
+import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import SignUp from "./pages/SignUp";
@@ -6,27 +6,78 @@ import Login from "./pages/Login";
 import ListingDetail from "./pages/ListingDetail";
 import Cart from "./pages/Cart";
 import VendorDashboard from "./pages/VendorDashboard";
-import CreateListing from './pages/CreateListing';
-import EditListing from './pages/EditListing';
-import Profile from './pages/Profile';
+import CreateListing from "./pages/CreateListing";
+import EditListing from "./pages/EditListing";
+import Profile from "./pages/Profile";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/signup" element={<SignUp />} />
+        {/* Public routes */}
         <Route path="/login" element={<Login />} />
-        <Route path="/listings/:id" element={<ListingDetail />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/dashboard" element={<VendorDashboard />} />
-        <Route path="/dashboard/create" element={<CreateListing />} />
-        <Route path="/dashboard/edit/:id" element={<EditListing />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route path="/signup" element={<SignUp />} />
 
+        {/* Protected routes */}
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Homepage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/cart"
+          element={
+            <ProtectedRoute>
+              <Cart />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/listings/:id"
+          element={
+            <ProtectedRoute>
+              <ListingDetail />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/profile"
+          element={
+            <ProtectedRoute>
+              <Profile />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <VendorDashboard />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/dashboard/create"
+          element={
+            <ProtectedRoute>
+              <CreateListing />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/dashboard/edit/:id"
+          element={
+            <ProtectedRoute>
+              <EditListing />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
 export default App;
