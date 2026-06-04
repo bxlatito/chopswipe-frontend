03349804ff2a3639e0b1bd0191fc207985d70fd4
Bolatito Olabode <bolaoluwatitoolabode@gmail.com>
2026-06-04 15:24:50 +0100

    .

--- src/components/SwipingButton.jsx
+++ src/components/SwipingButton.jsx
@@ -5,7 +5,7 @@ function SwipingButton({ direction, onClick }) {
     return (
         <button 
             onClick={onClick} 
-            className="border-2 border-black p-3 rounded-full hover:bg-red-300 transition-colors duration-300 text-2xl"
+            className="border-2 border-black p-3 rounded-full hover:bg-red-300 transition-colors duration-300 text-2xl bg-white"
         >
             {direction === 'left' ? <FaThumbsDown /> : <FaThumbsUp />}
         </button>
